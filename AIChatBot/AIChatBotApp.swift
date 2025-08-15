e04097d9104ac6382e4160ed73df5942eda8d50c
Nando <iframz13@gmail.com>
2025-08-15 06:51:21 -0700

    Initial Commit

---
+++ AIChatBot/AIChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatBotApp.swift
+//  AIChatBot
+//
+//  Created by Nando on 8/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
